e983a5c519bd3c03d5e3e0903b73dd811b9efe4f
Emma-Louise Moses <148122558+ErmIngenious@users.noreply.github.com>
2024-01-02 04:25:04 +0000

    Add files via upload

---
+++ Conversions.py
@@ -0,0 +1,27 @@
+''' declare the following variables then convert them:
+qt = 99.23 (convert to)  int
+qtb = 23  (convert to) float
+qt3 = 150     ''       string
+string1 = '100'   ''    int  
+
+output each one on seperate lines. 
+
+'''
+#decalring the variables and converting their data typs using casting. 
+#covert  float to int
+qt1 = int(99.23)   
+
+#convert int to float
+qt2 = float(23) 
+
+#convert int to string
+qt3 = str(150) 
+
+#convert string to int
+string1 = int('100') 
+
+#print each vartiable on a seperate line
+print (qt1)
+print (qt2)
+print (qt3)
+print (string1)
